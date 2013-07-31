ef0f0ba5b57fe083c48ba9daf846372350cabdcf
DOMINIQUE GEFFROY <geffroy.dominique@free.fr>
2013-07-31 22:11:00 +0200

    change raccourci xlock, et la resolution sur Everest

--- rc/xlock.lua
+++ rc/xlock.lua
@@ -9,7 +9,7 @@ xrun("xautolock",
 
 config.keys.global = awful.util.table.join(
    config.keys.global,
-   awful.key({}, "XF86ScreenSaver", function() awful.util.spawn("xautolock -locknow", false) end))
+   awful.key({modkey, }, "z", function() awful.util.spawn("xautolock -locknow", false) end))
 
 -- Configure DPMS
 os.execute("xset dpms 360 720 1200")
