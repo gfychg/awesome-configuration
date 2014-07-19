27c909d3737f1493a4e5bdf727da2199ce32aaa4
Dominique Geffroy <geffroy.dominique@free.fr>
2014-07-19 14:01:02 +0200

    lxterminal

--- rc.lua
+++ rc.lua
@@ -45,8 +45,9 @@ os.execute("test -d " .. awful.util.getdir("cache") ..
 -- Global configuration
 modkey = "Mod4"
 config = {}
-config.terminal = "urxvtcd --perl-lib " .. awful.util.getdir("config") .. "/lib/rxvt"
-config.termclass = "URxvt"
+--config.terminal = "urxvtcd --perl-lib " .. awful.util.getdir("config") .. "/lib/rxvt"
+config.terminal = "lxterminal"
+--config.termclass = "URxvt"
 config.layouts = {
    awful.layout.suit.tile,
    awful.layout.suit.tile.left,
